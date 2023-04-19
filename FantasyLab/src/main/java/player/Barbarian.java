25ffc52aed8ecdc79800c14c28e3939d281747a7
Lisa <84908214+LisaS7@users.noreply.github.com>
2023-04-19 13:36:41 +0100

    Created fighters

---
+++ FantasyLab/src/main/java/player/Barbarian.java
@@ -0,0 +1,7 @@
+package player;
+
+public class Barbarian extends Fighter{
+    public Barbarian(String name, int hp) {
+        super(name, hp);
+    }
+}
---
+++ FantasyLab/src/main/java/player/Dwarf.java
@@ -0,0 +1,7 @@
+package player;
+
+public class Dwarf extends Fighter{
+    public Dwarf(String name, int hp) {
+        super(name, hp);
+    }
+}
