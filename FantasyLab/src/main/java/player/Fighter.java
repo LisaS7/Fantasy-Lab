458cfb982fc32dbe299049f12e2e98828df7bde3
Lisa <84908214+LisaS7@users.noreply.github.com>
2023-04-19 13:36:55 +0100

    Created fighter abstract class

---
+++ FantasyLab/src/main/java/player/Fighter.java
@@ -0,0 +1,23 @@
+package player;
+
+import behaviours.IAttack;
+import behaviours.ITakeDamage;
+import enums.WeaponType;
+
+public abstract class Fighter extends Player implements IAttack {
+    private WeaponType weapon;
+
+    public Fighter(String name, int hp) {
+        super(name, hp);
+    }
+
+    @Override
+    public void attack(ITakeDamage target) {
+        int damage = this.weapon.getDamage();
+            target.takeDamage(damage);
+    }
+
+    public void changeWeapon(WeaponType weapon){
+        this.weapon = weapon;
+    }
+}
