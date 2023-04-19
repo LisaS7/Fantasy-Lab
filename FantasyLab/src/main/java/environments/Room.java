37a5076db51fa057cf88c5de410fbb1305e4186c
Lisa <84908214+LisaS7@users.noreply.github.com>
2023-04-19 15:59:39 +0100

    Added randomised exits to constructor

--- FantasyLab/src/main/java/environments/Room.java
+++ FantasyLab/src/main/java/environments/Room.java
@@ -3,7 +3,24 @@ package environments;
 import enums.Exit;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
 public class Room {
-    private ArrayList<Exit> exit = new ArrayList<>();
+    private ArrayList<Exit> exits = new ArrayList<>();
+
+    public Room() {
+        List<Exit> allExits = Arrays.asList(Exit.values());
+        Collections.shuffle(allExits);
+        int randomNumber = (int) (Math.random() * allExits.size());
+
+        for (int i = 0; i < randomNumber; i++) {
+            exits.add(allExits.get(i));
+        }
+    }
+
+    public ArrayList<Exit> getExits() {
+        return exits;
+    }
 }
