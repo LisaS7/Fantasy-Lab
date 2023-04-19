d6b56a1ac904dcc886ca453382bced7f0e0b4438
Lisa <84908214+LisaS7@users.noreply.github.com>
2023-04-19 14:10:00 +0100

    Created healType enum

--- FantasyLab/src/main/java/enums/HealType.java
+++ FantasyLab/src/main/java/enums/HealType.java
@@ -1,4 +1,16 @@
 package enums;
 
 public enum HealType {
+    POTION(18),
+    ELIXIR(25),
+    HERBS(5);
+
+
+    private final int healPoints;
+
+    HealType(int healPoints) {
+        this.healPoints = healPoints;
+    }
+
+    public int getHealPoints() {return healPoints;}
 }
