946432e6e06d46b1bd9d6736485667090d6cf666
Lisa <84908214+LisaS7@users.noreply.github.com>
2023-04-19 13:21:13 +0100

    Created enums

--- FantasyLab/src/main/java/enums/SpellType.java
+++ FantasyLab/src/main/java/enums/SpellType.java
@@ -1,4 +1,17 @@
 package enums;
 
 public enum SpellType {
+    CURSE(18),
+    WRATH(12),
+    FLARE(22);
+
+    private final int damage;
+
+    SpellType(int damage) {
+        this.damage = damage;
+    }
+
+    public int getDamage() {
+        return damage;
+    }
 }
--- FantasyLab/src/main/java/enums/WeaponType.java
+++ FantasyLab/src/main/java/enums/WeaponType.java
@@ -1,4 +1,18 @@
 package enums;
 
 public enum WeaponType {
+    SWORD(15),
+    CLUB(10),
+    AXE(12);
+
+    private final int damage;
+
+    WeaponType(int damage) {
+        this.damage = damage;
+    }
+
+    public int getDamage() {
+        return damage;
+    }
+
 }
