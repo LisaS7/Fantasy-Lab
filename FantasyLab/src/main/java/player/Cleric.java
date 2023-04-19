baac4f5e293afdedea269254635fe4df0ca52707
Lisa <84908214+LisaS7@users.noreply.github.com>
2023-04-19 14:21:41 +0100

    Improved Cleric to heal from random item

--- FantasyLab/src/main/java/player/Cleric.java
+++ FantasyLab/src/main/java/player/Cleric.java
@@ -3,13 +3,21 @@ package player;
 import behaviours.IHeal;
 import enums.HealType;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Cleric implements IHeal {
 
-    private HealType item;
+    private ArrayList<HealType> items;
 
     @Override
     public void heal(Player player) {
-        int healPoints = item.getHealPoints();
-        player.heal(healPoints);
+        int randomIndex = (int) (Math.random() * items.size());
+        HealType randomItem = items.get(randomIndex);
+        player.heal(randomItem.getHealPoints());
+    }
+
+    public void addItems(HealType ...items) {
+        this.items.addAll(List.of(items));
     }
 }
